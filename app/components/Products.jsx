696009394cceef6a8fa6a53c72b1fe98d56b4745
Karen Ziboh <karen@cchubnigeria.com>
2023-11-22 23:16:47 +0100

    update product section background

--- app/components/Products.jsx
+++ app/components/Products.jsx
@@ -1,5 +1,6 @@
 'use client'
 import ProductCard from "./ProductCard";
+import BackgroundVideo from "./BackgroundVideo";
 
 const Products = () => {
     const cardsLG = [
@@ -44,17 +45,21 @@ const Products = () => {
         },
     ]
     return (
-        <div className='w-full'>
-            <section className='xl:hidden max-w-[1920px] md:px-[30px] xl:px-[120px] 2xl:px-[240px] mx-auto overflow-scroll lg:overflow-hidden py-[76px] md:py-[204px] bg-black'>
+        <div className='w-full relative w-full h-max overflow-hidden'>
+            <div
+                className='bg-[#13171D] bg-opacity-60 xl:bg-black xl:hidden max-w-[1920px] relative z-50 md:px-[30px] xl:px-[120px] 2xl:px-[240px] mx-auto overflow-scroll lg:overflow-hidden py-[76px] md:py-[204px] bg-black'>
                 <div className='mx-auto flex gap-x-[40px] lg:gap-x-[20px] justify-between  px-[15px] w-max lg:w-full '>
                     {cards.map((card, index) => <ProductCard card={card} key={index}/>)}
 
                 </div>
-            </section>
-            <section className='pt-[176px] mx-auto hidden xl:flex gap-x-[40px] justify-between bg-black w-full py-[76px] px-[15px] md:px-[30px] md:px-[30px] xl:px-[120px] 2xl:px-[240px]'>
+            </div>
+            <section
+                className='pt-[176px] mx-auto hidden xl:flex gap-x-[40px] justify-between bg-black w-full py-[76px] px-[15px] md:px-[30px] md:px-[30px] xl:px-[120px] 2xl:px-[240px]'>
                 {cardsLG.map((card, index) => <ProductCard card={card} key={index}/>)}
             </section>
-
+            <div className='block xl:hidden'>
+                <BackgroundVideo/>
+            </div>
         </div>
     )
 }
