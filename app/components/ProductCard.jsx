004f785dfd324f07818e9d497dbf6f572fc17a46
Karen Ziboh <karen@cchubnigeria.com>
2023-11-23 10:21:43 +0100

    add transition to product app

--- app/components/ProductCard.jsx
+++ app/components/ProductCard.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 const ProductCard = ({card}) => {
     return (
         <div
-             className={`flex-1 relative pb-[20px] border-b border-[#13171D] py-[20px] pr-[20px] rounded-[6px] bg-[#13171D]`}>
+             className={`card-container flex-1 relative pb-[20px] border-b border-[#13171D] py-[20px] pr-[20px] rounded-[6px] bg-[#13171D]`}>
             <p className={`font-bold text-[12px] text-black w-max absolute right-[36px] top-[-10px] font-satoshi leading-[110%] text-center uppercase bg-white px-[6px] py-[3px] rounded-[100px]`}>COMING SOON</p>
             <p className={`ml-[20px] text-[22px] text-white font-monument uppercase leading-[120%] mb-[10px]`}>{card.heading}</p>
             <p className={`ml-[20px] gradient-text font-bold text-[18px] text-white font-satoshi leading-[130%] mb-[20px] max-w-[252px] `}>{card.subheading}</p>
@@ -11,9 +11,8 @@ const ProductCard = ({card}) => {
                 <Image
                     src={card.image}
                     alt='read more' width={272}
-                    height={234} className='object-cover w-full h-full'/>
+                    height={234} className='object-cover w-full h-full duration-300'/>
             </div>
-
             <p className={`pl-[20px] font-satoshi w-full text-[16px] text-white leading-[130%] pb-[30px] max-w-[252px] md:max-w-[382px] lg:max-w-max`}>
                 {card.text}
             </p>
