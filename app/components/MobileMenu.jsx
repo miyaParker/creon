8df0ef639fc2b90af0b66646014aa6e7e25c9143
Karen Ziboh <karen@cchubnigeria.com>
2023-12-02 07:08:21 +0100

    improves overall look of the website

--- app/components/MobileMenu.jsx
+++ app/components/MobileMenu.jsx
@@ -5,7 +5,8 @@ import {motion} from "framer-motion";
 const MobileMenu = () => {
     return (
         <div className='w-screen screen-h fixed bottom-0 left-0'>
-            <motion.div initial={{x: 208}} animate={{x: 0}} exit={{x: 208}} transition={{type: 'tween', ease:'easeOut', duration:0.25}}
+            <motion.div initial={{x: 208}} animate={{x: 0}} exit={{x: 208}}
+                        transition={{type: 'tween', ease: 'easeOut', duration: 0.25}}
                         className='screen-h top-0 absolute z-[150] right-0 w-[208px] bg-black h-full pt-[88px] pl-[30px]'>
                 <motion.ul className='flex flex-col text-white z-[390] relative'>
                     {menuItems.map((menuItem, index) => <li
@@ -20,22 +21,24 @@ const MobileMenu = () => {
                     </li>)}
                 </motion.ul>
                 <div className='absolute left-[30px] bottom-[30px] flex gap-x-[10px] items-center'>
-                        <div
-                            className='w-[34px] h-[34px] rounded-full border border-2 border-white flex  items-center justify-center hover:bg-[#3D8BFF] hover:border-transparent duration-200 ease-in cursor-pointer'>
-                            <a href="#"><Image src='/telegram.svg' width={16} height={16}/></a>
-                        </div>
-                        <div
-                            className='w-[34px] h-[34px] rounded-full border border-2 border-white flex  items-center justify-center hover:bg-[#3D8BFF] hover:border-transparent duration-200 ease-in cursor-pointer'>
-                            <a href="#"><Image src='/discord.svg' width={16} height={16}/></a>
-                        </div>
-                        <div
-                            className='w-[34px] h-[34px] rounded-full border border-2 border-white flex  items-center justify-center hover:bg-[#3D8BFF] hover:border-transparent duration-200 ease-in cursor-pointer'>
-                            <a href="#"><Image src='/twitter.svg' width={16} height={16}/></a>
-                        </div>
+                    <div
+                        className='w-[34px] h-[34px] rounded-full border border-2 border-white flex  items-center justify-center hover:bg-[#3D8BFF] hover:border-transparent duration-200 ease-in cursor-pointer'>
+                        <a href="#"><Image src='/telegram.svg' width={16} height={16}/></a>
+                    </div>
+                    <div
+                        className='w-[34px] h-[34px] rounded-full border border-2 border-white flex  items-center justify-center hover:bg-[#3D8BFF] hover:border-transparent duration-200 ease-in cursor-pointer'>
+                        <a href="#"><Image src='/discord.svg' width={16} height={16}/></a>
+                    </div>
+                    <div
+                        className='w-[34px] h-[34px] rounded-full border border-2 border-white flex  items-center justify-center hover:bg-[#3D8BFF] hover:border-transparent duration-200 ease-in cursor-pointer'>
+                        <a href="#"><Image src='/twitter.svg' width={16} height={16}/></a>
+                    </div>
                 </div>
             </motion.div>
-            <motion.div initial={{backdropFilter: 'none'}} animate={{backdropFilter: 'blur(5px) contrast(100%)'}} exit={{backdropFilter: 'none'}}
-                        className='absolute top-0 left-0  z-50  h-full w-screen bg-transparent '></motion.div>
+            <motion.div initial={{backdropFilter: 'none', backgroundColor: 'transparent'}}
+                        animate={{backdropFilter: 'blur(5px) contrast(100%)', backgroundColor: 'rgba(0,0,0,0.5)'}}
+                        exit={{backdropFilter: 'none', backgroundColor: "transparent"}}
+                        className='absolute top-0 left-0  z-50  h-full w-screen bg-transparent'></motion.div>
         </div>
     )
 }
