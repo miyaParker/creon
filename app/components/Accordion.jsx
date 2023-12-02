af89986598d13d692cad14c5eaed7698c0ffa8b1
Karen Ziboh <karen@cchubnigeria.com>
2023-12-02 09:48:17 +0100

    improves overall look of the website

--- app/components/Accordion.jsx
+++ app/components/Accordion.jsx
@@ -4,7 +4,7 @@ import {faqs} from "../constants";
 import {useState} from "react";
 
 const Accordion = () => {
-    const [panel, setPanel] =useState(0)
+    const [panel, setPanel] =useState(1)
     return (
         <section className='h-max lg:h-[800px] overflow-y-hidden w-full md:max-w-[472px] xl:max-w-[552px] relative z-20'>
             {faqs.map((faq, index) => <AccordionPanel panel={panel} setPanel={setPanel} data={faq} key={index} index={index+1}/>)}
--- app/components/AccordionPanel.jsx
+++ app/components/AccordionPanel.jsx
@@ -5,19 +5,20 @@ import {motion} from "framer-motion";
 const AccordionPanel = ({
                             data, index, panel, setPanel
                         }) => {
+    console.log(panel, index)
     return (
         <div onClick={() => {
-            if (panel === index + 1) {
+            if (panel === index) {
                 setPanel(0)
-            } else setPanel(index + 1)
+            } else setPanel(index)
         }}
              className={`${index === 1 ? '' : 'pt-[20px]'} relative pb-[20px] border-b border-[#13171D] cursor-pointer`}>
             <div className='flex items-center gap-[20px]'>
                 {data.icon(panel, index)}
-                <p className={`font-bold text-[18px] max-w-[323px] text-white font-satoshi text-[18px]leading-[130%]  ${panel === index + 1 ? '' : 'hover:text-[#3D8BFF] duration-200 delay-50 ease-linear'}`}>{data.q}</p>
+                <p className={`font-bold text-[18px] max-w-[323px] text-white font-satoshi text-[18px]leading-[130%]  ${panel === index ? '' : 'hover:text-[#3D8BFF] duration-200 delay-50 ease-linear'}`}>{data.q}</p>
             </div>
             <Image
-                className={`${panel === index + 1 ? '' : 'flip-horizontal duration-300'}  duration-300 cursor-pointer absolute top-[24px] right-[24px]`}
+                className={`${panel === index ? '' : 'flip-horizontal duration-300'}  duration-300 cursor-pointer absolute top-[24px] right-[24px]`}
                 src='/caret.svg'
                 alt='read more' width={14}
                 height={14}/>
@@ -28,13 +29,13 @@ const AccordionPanel = ({
                         }}
                         animate={{
                             height:
-                                panel === index + 1
+                                panel === index
                                     ? 'max-content'
                                     : 0,
-                            opacity: panel === index + 1 ? 1 : 0,
+                            opacity: panel === index ? 1 : 0,
                         }}
             >
-                <p className={`${panel === index + 1 ? 'block' : 'h-0'} ml-[93px] font-satoshi w-full text-[16px] text-white leading-[130%] pb-[30px] max-w-[252px] md:max-w-[379px]`}>
+                <p className={`${panel === index ? 'block' : 'h-0'} ml-[93px] font-satoshi w-full text-[16px] text-white leading-[130%] pb-[30px] max-w-[252px] md:max-w-[379px]`}>
                     {data.a}
                 </p>
             </motion.div>
